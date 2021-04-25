c9d2f61a8202678073bc1419b72a9c7381b2a63e
cphillips17 <55683870+cphillips17@users.noreply.github.com>
2021-04-24 22:22:00 -0500

    refactor about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,89 +1,88 @@
-// import React from "react";
-// import styled from "styled-components";
-// import { StaticImage } from "gatsby-plugin-image";
-// import Layout from "components/Layout";
-// import { Headline1, MediumBody, Headline2 } from "../components/Text";
-// import { JumboContainer, JumboText } from "../components/Jumbotron";
-// import { FullWidthContainer } from "../components/FullWidthContainer";
+import React from "react";
+import styled from "styled-components";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "components/Layout";
+import { Headline1, MediumBody, Headline2 } from "../components/Text";
+import { Jumbotron, JumboText, Row } from "../components/Blocks";
 
 
-// const AboutPeopleContainer = styled.div`
-//   display: flex;
-//   position: relative;
-//   flex-direction: column;
-//   margin: ${props => `2vw ${props.theme.pageMargin}`};
-//   background-color: ${props => props.theme.color.mediumLight};
-// `
+const AboutPeopleContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  margin: ${props => `2vw ${props.theme.pageMargin}`};
+  background-color: ${props => props.theme.color.mediumLight};
+`
 
-// const AboutPeopleGrid = styled.div`
-//   flex: 1;
-//   display: grid; 
-//   padding: .5vw 0;
-//   border-top: 1px solid black;
-// `
+const AboutPeopleGrid = styled.div`
+  flex: 1;
+  display: grid; 
+  padding: .5vw 0;
+  border-top: 1px solid black;
+`
 
-// const JumboPicture = styled.div`
-//   position: relative;
-//   flex: 1;
-// `
+const JumboPicture = styled.div`
+  position: relative;
+  flex: 1;
+`
 
-// function About() {
-//   return (
-//     <Layout title="About RAIL">
-//         <JumboContainer>
-//           <JumboText>
-//             <Headline1>
-//               About RAI
-//             </Headline1>
-//             <MediumBody>
-//             AI Global is a non-profit organization building tangible governance tools for trustworthy, safe, and fair Artificial Intelligence (AI). Through a first-of-its-kind certification system that qualifies AI systems, we support practitioners as they navigate the complex landscape of creating Responsible AI. <br/> Feedback generated from these systems will in turn inform AI policymakers, enabling technologies that improve the social and economic well-being of society. AI Global brings extensive experience in responsible AI policy and is uniquely positioned to partner with organizations across public and private sectors to guide and inform responsible AI governance around the world.
-//             </MediumBody>
-//           </JumboText>
-//             {/* <StaticImage
-//               src="../images/pictures/about/jumboforeground.png"
-//               style={{
-//                 position: "absolute",
-//                 zIndex: "4",
-//                 right: "0",
-//                 top: "134px",
-//               }}
-//               width={1479}
-//               height={709}
-//               alt="jumbo foreground"
-//             />
-//             <StaticImage
-//               src="../images/pictures/about/jumbobackground.png"
-//               style={{
-//                 position: "absolute",
-//                 zIndex: "3",
-//                 right: "0",
-//                 top: "162px",
-//               }}
-//               width={1177}
-//               height={618}
-//               alt="jumbo background"
-//             /> */}
-//       </JumboContainer>
-//       <FullWidthContainer>
-//         <AboutPeopleContainer>
-//           <Headline2>
-//             Board
-//           </Headline2>
-//           <AboutPeopleGrid>
+function About() {
+  return (
+    <Layout title="About RAIL">
+      <Jumbotron>
+        <JumboText>
+          <Headline1>About RAI</Headline1>
+          <MediumBody>
+            AI Global is a non-profit organization building tangible governance
+            tools for trustworthy, safe, and fair Artificial Intelligence (AI).
+            Through a first-of-its-kind certification system that qualifies AI
+            systems, we support practitioners as they navigate the complex
+            landscape of creating Responsible AI. <br /> Feedback generated from
+            these systems will in turn inform AI policymakers, enabling
+            technologies that improve the social and economic well-being of
+            society. AI Global brings extensive experience in responsible AI
+            policy and is uniquely positioned to partner with organizations
+            across public and private sectors to guide and inform responsible AI
+            governance around the world.
+          </MediumBody>
+        </JumboText>
+        {/* <StaticImage
+              src="../images/pictures/about/jumboforeground.png"
+              style={{
+                position: "absolute",
+                zIndex: "4",
+                right: "0",
+                top: "134px",
+              }}
+              width={1479}
+              height={709}
+              alt="jumbo foreground"
+            />
+            <StaticImage
+              src="../images/pictures/about/jumbobackground.png"
+              style={{
+                position: "absolute",
+                zIndex: "3",
+                right: "0",
+                top: "162px",
+              }}
+              width={1177}
+              height={618}
+              alt="jumbo background"
+            /> */}
+      </Jumbotron>
+      <Row>
+        <AboutPeopleContainer>
+          <Headline2>Board</Headline2>
+          <AboutPeopleGrid></AboutPeopleGrid>
+        </AboutPeopleContainer>
+        <AboutPeopleContainer>
+          <Headline2>Nice</Headline2>
+          <AboutPeopleGrid></AboutPeopleGrid>
+        </AboutPeopleContainer>
+      </Row>
+    </Layout>
+  );
+}
 
-//           </AboutPeopleGrid>
-//         </AboutPeopleContainer>
-//         <AboutPeopleContainer>
-//           <Headline2>
-//             Nice
-//           </Headline2>
-//           <AboutPeopleGrid>
-
-//           </AboutPeopleGrid>
-//         </AboutPeopleContainer>
-//       </FullWidthContainer>
-//     </Layout>
-//   );
-// }
-
-// export default About;
+export default About;
