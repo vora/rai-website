4e3bbd37eed7f7242a36298ffbc4b56270935c07
cphillips17 <55683870+cphillips17@users.noreply.github.com>
2021-04-27 19:24:17 -0500

    news page + share

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,5 +52,13 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: "gatsby-plugin-social9-socialshare",
+      options: {
+        content: "a2e31cdba944465fa43b468a80af7af3",
+        async: true,
+        defer: true,
+      },
+    },
   ],
 };
