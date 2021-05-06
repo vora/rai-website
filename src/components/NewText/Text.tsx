c5e106c3e4859c92ae072b0e3214ae61908cc293
eddysims <eddy.s@getjobber.com>
2021-05-05 21:41:55 -0600

    adds the new Text component

---
+++ src/components/NewText/Text.tsx
@@ -0,0 +1,17 @@
+import React, { PropsWithChildren } from "react";
+import classnames from "classnames";
+
+import * as styles from "./Text.module.css";
+
+interface TextProps {
+  readonly size?: "base" | "small";
+}
+
+export function Text({
+  size = "base",
+  children,
+}: PropsWithChildren<TextProps>) {
+  const textClass = classnames(styles.text, styles[size]);
+
+  return <p className={textClass}>{children}</p>;
+}
