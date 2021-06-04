9a2d2e5b18739df651bf4ba126eb0d7b83fdf045
eddysims <eddy.s@getjobber.com>
2021-06-03 21:54:19 -0600

    adds md linting and lints markdown

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   "*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"],
   "*.{js,jsx,ts,tsx}": ["npm run lint:js", "jest --bail --findRelatedTests"],
+  "*.{md,mdx}": ["yarn lint:md"],
 };
