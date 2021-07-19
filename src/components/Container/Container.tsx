762085c1de6c869459f0305de49d0788f7db4ed3
eddysims <eddy.s@getjobber.com>
2021-07-19 09:50:12 -0600

    clean up Container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,24 +5,15 @@ import styles from "./Container.module.css";
 
 interface ContainerProps {
   readonly className?: string;
-  readonly flex?: boolean;
-  readonly justifyContent?: "flexStart" | "spaceBetween";
-  readonly alignItems?: "center" | "flexStart";
   readonly size?: "base" | "small";
 }
 
 export function Container({
   children,
   className,
-  flex,
-  justifyContent = "flexStart",
-  alignItems = "center",
   size = "base",
 }: PropsWithChildren<ContainerProps>) {
   const conatinerClass = classnames(styles.container, className, {
-    [styles.flex]: flex,
-    [styles.justifySpaceBetween]: justifyContent === "spaceBetween",
-    [styles.alignItemsCenter]: alignItems === "center",
     [styles.small]: size === "small",
   });
 
