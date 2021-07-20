c027a56d75514cde67521ade3592b9a960f15c92
eddysims <eddy.s@getjobber.com>
2021-07-20 12:21:19 -0600

    document Caption component

--- src/components/Caption/Caption.tsx
+++ src/components/Caption/Caption.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import styles from "./Caption.module.css";
 
 interface CaptionProps {
+  /**
+   * Text to be displayed in the Caption. Reserved for short
+   * amounts of text
+   */
   readonly title: string;
 }
 
