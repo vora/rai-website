6459b472da19986a9f9c24dad4d861eeb0b71137
eddysims <eddy.s@getjobber.com>
2021-08-20 16:56:49 -0600

    adds a proper 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Layout } from "@/components/Layout";
+import { EmptyPage } from "@/components/EmptyPage";
+import { Text } from "@/components/Text";
+import { Heading } from "@/components/Heading";
+import { Container } from "@/components/Container";
+
+function NotFound() {
+  return (
+    <Layout title="404 - Page not found">
+      <EmptyPage>
+        <Container size="small">
+          <Text size="large">🤖</Text>
+          <Heading>Page not found</Heading>
+          <Text>
+            It seems the page you are looking for can not be found. Please check
+            the URL and try again.
+          </Text>
+        </Container>
+      </EmptyPage>
+    </Layout>
+  );
+}
+
+export default NotFound;
