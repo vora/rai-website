0fe398e937f02432b816d38581925c28fab1de02
eddysims <eddy.s@getjobber.com>
2021-08-25 17:30:33 -0600

    add dark mode

--- src/blocks/Content/Content.module.css.d.ts
+++ src/blocks/Content/Content.module.css.d.ts
@@ -1,8 +1,10 @@
 declare const styles: {
   readonly "fullwidth": string;
-  readonly "encapsulated": string;
+  readonly "fullwidthdark": string;
   readonly "container": string;
+  readonly "encapsulated": string;
   readonly "button": string;
+  readonly "image": string;
 };
 export = styles;
 
--- src/blocks/Content/Content.tsx
+++ src/blocks/Content/Content.tsx
@@ -5,6 +5,7 @@ import { ContentFragmentFragment } from "@/graphql/graphql-types";
 import { Container } from "@/components/Container";
 import { RichText, RichTextContent } from "@/components/RichText";
 import { Button } from "@/components/Button";
+import { Image } from "@/components/Image";
 
 import styles from "./Content.module.css";
 
@@ -13,9 +14,9 @@ interface ContentProps {
 }
 
 export function Content({
-  data: { variation, content, customButtonText, buttonLink },
+  data: { variation, content, customButtonText, buttonLink, image },
 }: ContentProps) {
-  const cssVariation = variation?.toLowerCase().replace(" ", "");
+  const cssVariation = variation?.toLowerCase().replace(/ /g, "");
 
   // @ts-expect-error we can expect the classname to exist here
   const wrapperClass = classNames(styles.wrapper, styles[cssVariation]);
@@ -24,18 +25,25 @@ export function Content({
     <div className={wrapperClass}>
       <Container>
         <div className={styles.container}>
-          {content && <RichText content={content as RichTextContent} />}
-          {buttonLink && (
-            <div className={styles.button}>
-              <Button
-                title={customButtonText ?? (buttonLink?.title as string)}
-                url={
-                  buttonLink.__typename === "ContentfulPage"
-                    ? `/${buttonLink?.slug}`
-                    : (buttonLink?.url as string)
-                }
-                external={buttonLink.__typename === "ContentfulResource"}
-              />
+          <div>
+            {content && <RichText content={content as RichTextContent} />}
+            {buttonLink && (
+              <div className={styles.button}>
+                <Button
+                  title={customButtonText ?? (buttonLink?.title as string)}
+                  url={
+                    buttonLink.__typename === "ContentfulPage"
+                      ? `/${buttonLink?.slug}`
+                      : (buttonLink?.url as string)
+                  }
+                  external={buttonLink.__typename === "ContentfulResource"}
+                />
+              </div>
+            )}
+          </div>
+          {image && (
+            <div className={styles.image}>
+              <Image {...image} />
             </div>
           )}
         </div>
@@ -64,6 +72,11 @@ export const ContentFragment = graphql`
         url
         title
       }
+    }
+    image {
+      title
+      description
+      gatsbyImageData(width: 500, placeholder: BLURRED, formats: [AUTO, WEBP])
     }
   }
 `;
