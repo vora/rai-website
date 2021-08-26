520da818a1f2fcfa42ddf1524e770fa036be89e7
eddysims <eddy.s@getjobber.com>
2021-08-26 17:05:08 -0600

    adds custom icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,21 +1,43 @@
 import React from "react";
 import * as Icons from "react-feather";
+import { customIcons } from "./customIcons";
 
-export type IconType = keyof typeof Icons;
+export type IconType = keyof typeof Icons | keyof typeof customIcons;
 
 interface IconProps {
   readonly icon: IconType;
-  readonly size?: "small" | "base" | "large";
+  readonly size?: "small" | "base" | "large" | "largest" | "humongous";
 }
 
 export function Icon({ icon, size = "base" }: IconProps) {
-  const Element = Icons[icon];
+  const isCustomIcon = Object.keys(customIcons).includes(icon);
 
   const sizes = {
     small: 16,
     base: 24,
     large: 32,
+    largest: 125,
+    humongous: 200,
   };
+
+  if (isCustomIcon) {
+    const customIcon = customIcons[icon as keyof typeof customIcons];
+
+    return (
+      <svg
+        width={sizes[size]}
+        height={sizes[size]}
+        viewBox={customIcon.viewbox}
+        data-testid={`icon-${icon}`}
+      >
+        {customIcon.paths.map((path) => (
+          <path key={path.d} d={path.d} />
+        ))}
+      </svg>
+    );
+  }
+
+  const Element = Icons[icon as keyof typeof Icons];
 
   return <Element size={sizes[size]} data-testid={`icon-${icon}`} />;
 }
